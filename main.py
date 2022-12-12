8664ebe8ae6dc4061c8287d8712516c24c594b20
Jesse Calhoun <jessecalhoun@Jesses-MacBook-Air.local>
2022-12-12 12:06:21 -0500

    Created satisified() function, along with if,else statement to determine whther satisfied or something needs to change. Also changed initial format for first full trip print

--- main.py
+++ main.py
@@ -15,12 +15,11 @@ modes_of_transportation = ['Uber', 'Limo Service', 'Sports Car', 'Bus']
 list_of_events = [destinations, restaurants, forms_of_entertainment, modes_of_transportation]
 
 def run_day_trip_generator():
-    city = random.choice(destinations)
+    destination = random.choice(destinations)
     restaraunt = random.choice(restaurants)
     form_of_entertainment = random.choice(forms_of_entertainment)
     mode_of_transport = random.choice(modes_of_transportation)
-    return (f'''Here's your trip!
-City: {city}
+    return (f'''City: {destination}
 Restaurant: {restaraunt}
 Form of entertainment: {form_of_entertainment}
 Mode of Transport: {mode_of_transport}''')
@@ -29,7 +28,22 @@ Mode of Transport: {mode_of_transport}''')
 
 current_trip = run_day_trip_generator()
 
-print(current_trip)
+print(f'''Here's your trip!
+{current_trip}''')
+print()
+def satisfied(list_of_events):
+    list_of_events = [destinations, restaurants, forms_of_entertainment, modes_of_transportation]
+    response = input('Are you satisfied with this trip? y/n ')
+    return response 
+print()
+user_response = satisfied(current_trip)
 
-#def satisfied(current_trip, list_of_events):
-#    if current_trip == 
+if user_response == 'y':
+    print()
+    print(f'''Here's your final trip!
+    {current_trip}''')
+else:
+    print()
+    input('What would you like to change about your trip? ')
+
+
