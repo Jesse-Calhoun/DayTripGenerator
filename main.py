a2a4a057e7958a839036c0b3d54ff823d96bbaf9
Jesse Calhoun <jessecalhoun@Jesses-MacBook-Air.local>
2022-12-12 17:33:46 -0500

    Changed the run_day_trip_generator() function to day_trip_generator() function and added new run_day_trip_generator() function that will run the day_trip_generator() function and the satisfied() function inside itself. Also worked on the while loop and iif/elif/else conditional inside of the satisfied() function to have the properly replace the previous random results for their particular list. git push git status

--- main.py
+++ main.py
@@ -12,38 +12,43 @@ destinations = ['New York City', 'Dallas', 'Washington D.C.', 'Philadelphia']
 restaurants = ['Steakhouse', 'Mcdonalds', 'Del Taco', 'Hibachi']
 forms_of_entertainment = ['NFL game', 'Comedy Show', 'Concert', 'Nightclub']
 modes_of_transportation = ['Uber', 'Limo Service', 'Sports Car', 'Bus']
-list_of_events = [destinations, restaurants, forms_of_entertainment, modes_of_transportation]
 
 def run_day_trip_generator():
+    results = day_trip_generator()
+    print(results)
+    results = satisfied(results)
+    print(results)
+    satisfied(results)
+
+def day_trip_generator():
     destination = random.choice(destinations)
     restaraunt = random.choice(restaurants)
     form_of_entertainment = random.choice(forms_of_entertainment)
     mode_of_transport = random.choice(modes_of_transportation)
-    return (f'''City: {destination}
-Restaurant: {restaraunt}
-Form of entertainment: {form_of_entertainment}
-Mode of Transport: {mode_of_transport}''')
+    return [destination, restaraunt, form_of_entertainment, mode_of_transport]
 
-#run_day_trip_generator()
-
-current_trip = run_day_trip_generator()
-
-print(f'''Here's your trip!
-{current_trip}''')
-print()
 def satisfied(list_of_events):
-    list_of_events = [destinations, restaurants, forms_of_entertainment, modes_of_transportation]
-    response = input('Are you satisfied with this trip? y/n ')
-    return response 
-print()
-user_response = satisfied(current_trip)
+    print()
+    response = input('Are you satisfied with this trip? y/n ') 
+    print()
+    while response == 'n':
+        change_request = input('What would you like to change about your trip? type done when finished. ')
+        if change_request == 'destination':
+            list_of_events[0] = random.choice(destinations)
+        elif change_request == 'restaurant':
+            list_of_events[1] = random.choice(restaurants)
+        elif change_request == 'form of entertainment':
+            list_of_events[2] = random.choice(forms_of_entertainment)
+        elif change_request == 'mode of transport':
+            list_of_events[3] = random.choice(modes_of_transportation)
+        elif change_request == 'done':
+            return list_of_events
+        print(list_of_events)
+    else:
+        print(f'''Here's your final trip!
+ {list_of_events}''')    
 
-if user_response == 'y':
-    print()
-    print(f'''Here's your final trip!
-    {current_trip}''')
-else:
-    print()
-    input('What would you like to change about your trip? ')
+run_day_trip_generator()
+
 
 
